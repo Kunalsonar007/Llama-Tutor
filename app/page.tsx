74df2dd6c5e06ed2c16d4c10537d45e1bad6eea2
Kunalsonar007 <kunalwankhede958@gmail.com>
2024-07-30 20:48:14 +0530

    New

--- app/page.tsx
+++ app/page.tsx
@@ -134,7 +134,7 @@ export default function Home() {
       <Header />
 
       <main
-        className={`flex grow flex-col px-4 pb-4 ${showResult ? "overflow-hidden" : ""}`}
+        className={`flex grow flex-col pb-4 ${showResult ? "overflow-hidden" : ""}`}
       >
         {showResult ? (
           <div className="mt-2 flex w-full grow flex-col justify-between overflow-hidden">
